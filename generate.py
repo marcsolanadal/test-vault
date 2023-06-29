29bc002569533af7af5cfa4d27c60bd308973d5b
Marc Solà Nadal <mail@marcsolanadal.com>
2023-06-29 18:39:19 +0200

    feat: add build steps

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import re
 from obsidian_to_hugo import ObsidianToHugo
 
 content_path = './hugo/content'
@@ -15,10 +16,14 @@ def filter_file(file_contents: str, file_path: str) -> bool:
     else:
         return False # skip file
 
+def remove_comment(file_contents: str) -> str:
+    return re.sub(r"%%.*?%%", "", file_contents, flags=re.DOTALL)
+
 obsidian_to_hugo = ObsidianToHugo(
-    obsidian_vault_dir=".",
+    obsidian_vault_dir="./notes",
     hugo_content_dir=content_path,
     filters=[filter_file],
+    processors=[remove_comment],
 )
 
 obsidian_to_hugo.run()
